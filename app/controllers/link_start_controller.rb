974909ab5fa60ad552f0b81efa4331d0a7c9b104
Elirudite <elijahclaude@gmail.com>
2016-04-12 10:09:01 -0400

    My first real app in Rails. A theoretical 'travel app' Please dont take any of this seriously

---
+++ app/controllers/link_start_controller.rb
@@ -0,0 +1,15 @@
+class LinkStartController < ApplicationController
+  def index
+  	@homestate = "Mortal Realm"
+  	@countries = ["Skyrim", "Morrowind", "Riverrun", "Shadowrealm"]
+  	@images = ["futurecity.jpg","magical-city.jpg","trekwars.jpg","coolscape.png"]
+
+  	@images2 = {"Future City" => "futurecity.jpg", "Magical City -- Thar be Magic" => "magical-city.jpg", 
+  		"Mashup of Star Trek and Star Wars" => "trekwars.jpg",  "Cool Landscape"=> "coolscape.png"}
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
---
+++ app/helpers/link_start_helper.rb
@@ -0,0 +1,2 @@
+module LinkStartHelper
+end
