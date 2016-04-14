023f208e1c2a05461978bf53125b0720aa03209e
Elirudite <elijahclaude@gmail.com>
2016-04-14 11:45:50 -0400

    Google API! carrierwave, geocoder, and figaro gems.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413145343) do
+ActiveRecord::Schema.define(version: 20160414150137) do
 
   create_table "destinations", force: :cascade do |t|
     t.string   "city"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20160413145343) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "population"
+    t.string   "avatar"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
